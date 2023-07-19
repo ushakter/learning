e1286159a2b634e95a354ff42884f0ccba513c6d
ushakter <ushakter.dev@gmail.com>
2023-07-19 22:33:05 +0600

    java script

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-function alertMessage () {
-    alert('Hello')
-}
+let a,b,c;
+        a=6;
+        b=6;
+        c=a+b;
+    document.getElementById("one").innerHTML= c;
 
-function getMessage(a, b) {
-    document.getElementById('one').innerHTML = a+b
-}
 
 
